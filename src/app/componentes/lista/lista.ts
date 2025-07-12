564f4629b1e22860d37e8bc91da45b70c97c8b59
JoaquinStechina <joaquinstechina12@gmail.com>
2025-07-12 16:01:21 -0300

    feat: added input with button

--- src/app/componentes/lista/lista.ts
+++ src/app/componentes/lista/lista.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista',
-  imports: [],
+  imports: [MatButtonModule, FormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './lista.html',
   styleUrl: './lista.css',
 })
