36afc548f2cff4c6bd3f559c93353d1f614e6472
JoaquinStechina <joaquinstechina12@gmail.com>
2025-08-07 15:23:38 -0300

    feat: added checkboxes to table

--- src/app/componentes/lista/lista.ts
+++ src/app/componentes/lista/lista.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { SelectionModel } from '@angular/cdk/collections';
 
 export interface Tarea {
   posicion: number;
   nombre: string;
-  descripicion: string;
 }
 
 @Component({
@@ -22,27 +23,55 @@ export interface Tarea {
     MatInputModule,
     MatFormFieldModule,
     MatTableModule,
+    MatCheckboxModule,
   ],
   templateUrl: './lista.html',
   styleUrl: './lista.css',
 })
 export class Lista implements OnInit {
-  public displayedColumns: string[] = ['posicion', 'nombre', 'descripcion'];
-
+  public displayedColumns: string[] = ['select', 'posicion', 'nombre'];
   public nuevaTarea: string = '';
-
-  public tareas: Tarea[] = [];
-
+  private tareas: Tarea[] = [];
+  public dataSource = new MatTableDataSource<Tarea>(this.tareas);
+  public selection = new SelectionModel<Tarea>(true, []);
   public isShown: boolean = true;
 
   ngOnInit(): void {
     this.cargarTareas();
   }
 
+  /** Whether the number of selected elements matches the total number of rows. */
+  public isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  public toggleAllRows(): void {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  public checkboxLabel(row?: Tarea): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.posicion + 1
+    }`;
+  }
+
   private cargarTareas(): void {
     if (typeof window !== 'undefined' && localStorage) {
       const tareasGuardadas = localStorage.getItem('tareas');
       this.tareas = tareasGuardadas ? JSON.parse(tareasGuardadas) : [];
+      this.dataSource.data = this.tareas;
     }
   }
 
@@ -55,13 +84,13 @@ export class Lista implements OnInit {
       const nuevaTarea: Tarea = {
         posicion: this.tareas.length + 1,
         nombre: this.nuevaTarea,
-        descripicion: '',
       };
       this.tareas.push(nuevaTarea);
       if (typeof window !== 'undefined' && localStorage) {
         localStorage.setItem('tareas', JSON.stringify(this.tareas));
       }
       this.nuevaTarea = '';
+      this.cargarTareas();
     }
   }
 }
